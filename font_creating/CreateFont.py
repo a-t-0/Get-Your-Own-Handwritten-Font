48aa93651c3ab62225d4f4bb193eadcd797a845d
a-t-0 <eai@protonmail.com>
2020-08-21 23:01:41 +0200

    Included test to see if file already dlded and unzipped.

--- font_creating/CreateFont.py
+++ font_creating/CreateFont.py
@@ -1,40 +1,18 @@
 # conda install -c conda-forge fonttools
+import os
+
+# runs a fontforge command to generate an actual font based on the .svg and .json files in template_reading/font_in/
 class CreateFont():
-    def __init__(self):
-        import os
-        #os.system('cmd /k "fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "template_reading/font_in/example.json"')
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "template_reading/font_in/example.json"')
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "/font_creating/svgs2ttf.py" "/template_reading/font_in/example.json"')
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "/font_creating/svgs2ttf.py" "../template_reading/font_in/example.json"')
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "../font_creating/svgs2ttf.py" "../template_reading/font_in/example.json"')
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "../template_reading/font_in/example.json"')
-        import os
-        current= os.path.dirname(os.path.abspath(__file__))
-        current= os.getcwd()
+    def __init__(self):        
+        current= os.getcwd() # gets path of parent script that is running
         path_to_fontforge= f'{current}/fontforge_portable/fontforge.bat'
         path_to_script = f'{current}/font_creating/svgs2ttf.py' 
         path_to_json = f'{current}/template_reading/font_in/example.json'
        
-        print(f'current={current}')
-        print(f'path_to_fontforge={path_to_fontforge}')
-        print(f'isfile={os.path.isfile(path_to_fontforge)}')
-        print(f'isfile={os.path.isfile(path_to_script)}')
-        print(f'isfile={os.path.isfile(path_to_json)}')
         command = f'cmd /k "{path_to_fontforge}" -lang=py -script {path_to_script} {path_to_json}'
         print(f'command={command}')
         os.system(command)
-        #os.system('cmd /k "/fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "template_reading/font_in/example.json"')
         
 if __name__ == '__main__':
     main = CreateFont()
 
-#fontforge_portable/fontforge -lang=py -script svgs2ttf.py template_reading/font_in/example.json
-#fontforge_portable/fontforge --version
-#"fontforge_portable/fontforge" --version
-#"fontforge_portable/fontforge" -lang=py -script svgs2ttf.py template_reading/font_in/example.json"
-#"fontforge_portable/fontforge" -lang=py -script "../font_creating/svgs2ttf.py" template_reading/font_in/example.json"
-#"fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "template_reading/font_in/example.json"
-#"fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "/template_reading/font_in/example.json"
-#"fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "../template_reading/font_in/example.json"
-
-#"fontforge_portable/fontforge" -lang=py -script "font_creating/svgs2ttf.py" "template_reading/font_in/example.json"
--- installMiktex.py
+++ installMiktex.py
@@ -14,7 +14,7 @@ import subprocess
 # miktexsetup --verbose --local-package-repository=./tempfolder --package-set=complete download
 
 #https://miktex.org/faq/setup-cli
-#miktexsetup --package-set=basic download
+#miktexsetup --package-set=basic download (did not do this, chose second command)
 #miktexsetup install
 
 class InstallMiktex:
@@ -22,27 +22,39 @@ class InstallMiktex:
     # intializes object
     def __init__(self):
         self.miktex_portable_dir = "./miktex_portable"
-        url = 'https://miktex.org/download/win/miktexsetup-x64.zip'
-        wget.download(url) 
+        self.zipfile = 'miktexsetup-x64.zip'
+        self.url = f'https://miktex.org/download/win/{self.zipfile}'
+        if not self.check_if_zip_exists():
+            wget.download(self.url) 
         
-        import os
+        
+    def install_miktex(self):
         current= os.path.dirname(os.path.abspath(__file__))
         path_to_miktex= f'{current}/miktex_portable/miktexsetup.exe'
         print(f'current={current}')
         print(f'path_to_miktex={path_to_miktex}')
         print(f'isfile={os.path.isfile(path_to_miktex)}')
         os.system(f'cmd /k "{path_to_miktex}" install')
+        print(f'COMPLETED MIKTEX INSTALLTION YOU CAN NOW COMPILE THE LATEX!')
         
+    def check_if_zip_exists(self):
+        current= os.path.dirname(os.path.abspath(__file__))
+        foundfile= os.path.isfile(f'{current}/{self.zipfile}')
+        return foundfile
         
     # walks through zip files and calls function to unpack the zip file
-    def walk_through_zip_files(self,folder):
+    def extract_zipfile(self,folder):
         files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        current= os.path.dirname(os.path.abspath(__file__))
         for f in files:
             if f[-4:]==".zip":
-                print('unsipping')
-                with zipfile.ZipFile(f, 'r') as zip_ref:
-                    zip_ref.extractall()
-                print(f' done converting')
+                if not os.path.isfile(f'{current}/miktexsetup.exe'):
+                    print('unzipping')
+                    with zipfile.ZipFile(f, 'r') as zip_ref:
+                        zip_ref.extractall()
+                    print(f' done unzipping')
+                else:
+                    print(f'miktex zip already unzipped into miktexsetup.exe')
     
     def make_miktex_portable_directory(self):
         import os
@@ -62,6 +74,7 @@ class InstallMiktex:
 if __name__ == '__main__':
        
     main = InstallMiktex()
-    # main.walk_through_zip_files("./")
-    # main.make_miktex_portable_directory()
-    # main.install_miktex_portable()
+    main.extract_zipfile("./")
+    main.make_miktex_portable_directory()
+    main.install_miktex_portable()
+    main.install_miktex()
